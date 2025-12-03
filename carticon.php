dfefd07af5e8066513c90422e77999be027fe221
Sumit Poudel <sumitpoudel325@gmail.com>
2025-12-03 20:02:24 +0545

    cart icon redone

---
+++ carticon.php
@@ -0,0 +1,30 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "medpointdb");
+session_start();
+if (isset($_GET["username"])) {
+    if ($_GET["username"] != $_SESSION["username"]) {
+        exit();
+    }
+    $username = $_GET["username"];
+    $sql = "SELECT COUNT(*) FROM tbcart WHERE username = '$username' AND isdelivered = 0";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        echo $row["COUNT(*)"];
+        exit();
+    } else {
+        echo 0;
+        exit();
+    }
+}
+?>
+
+    <div class="fixed bottom-10 right-10 w-12 h-12">
+        <a href="profile.php" class="relative hover:cursor-pointer">
+        <img class="w-full h-full rounded-full bg-white shadow-md p-2" src="./public/cart.svg" alt="">
+
+        <strong id="cartNum" class="absolute -top-2 -right-2 bg-red-500 text-white text-sm w-6 h-6 flex items-center justify-center rounded-full">
+            0
+        </strong>
+        </a>
+    </div>
--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-define("BASE_URL", "http://localhost/medpoint"); ?>
-
 <?php include "header.php"; ?>
 <body>
   <?php include "navbar.php"; ?>
@@ -11,7 +8,46 @@ define("BASE_URL", "http://localhost/medpoint"); ?>
     <div id="searchResult"></div>
     <?php
     include "items.php";
-    include "cart.php";
+    include "carticon.php";
     ?>
   </main>
-  <?php include "footer.php"; ?>
+  <footer class=" w-full bg-med-drklime text-white">
+    <div class="w-full p-4 flex justify-around">
+      <div>
+        <h3 class="font-semibold  mb-3.5">Contact</h3>
+        <p class=" gap-2.5 mb-2">
+          <img src="./public/location.svg" class="h-4 w-4 inline-block " alt="location"> Ramailo-Chowk, Chitwan
+        </p>
+        <p class="gap-2.5 mb-2">
+          <img src="./public/mail.svg" class="h-4 w-4 inline-block " alt="mail"> sumitpoudel@gmail.com
+        </p>
+        <p class="gap-2.5 mb-2">
+          <img src="./public/mail.svg" class="h-4 w-4 inline-block " alt="mail"> bimalmagar@gmail.com
+        </p>
+      </div>
+      <div>
+        <h3 class="font-semibold  mb-3.5">Social Media</h3>
+        <div class="flex gap-4 text-2xl">
+          <a href="#" class="hover:text-blue-400">🌐</a>
+          <a href="#" class="hover:text-pink-500">📸</a>
+          <a href="#" class="hover:text-blue-400"> 💼</a>
+          <a href="#" class="hover:text-red-600"> ▶</a>
+        </div>
+      </div>
+    </div>
+    <div class="w-full p-2 flex justify-around">
+      <p>© 2025 SUMIT. All rights reserved.</p>
+      <div class="flex gap-4">
+        <p>
+          Terms and Conditions
+        </p>
+        <p>
+          Privacy Policy
+        </p>
+      </div>
+    </div>
+  </footer>
+  <script src="public/js/index.js"></script>
+  <script src="public/js/nav.js"></script>
+  </body>
+  </html>
